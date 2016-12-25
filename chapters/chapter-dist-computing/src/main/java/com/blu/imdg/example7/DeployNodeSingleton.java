9cee8fd1e2b2fc3c4da35490ac627442b3e9280f
shamim <shamimapache@gmail.com>
2016-12-25 11:11:41 +0300

    add node singleton

---
+++ chapters/chapter-dist-computing/src/main/java/com/blu/imdg/example7/DeployNodeSingleton.java
@@ -0,0 +1,32 @@
+package com.blu.imdg.example7;
+
+import com.blu.imdg.common.CommonConstants;
+import com.blu.imdg.common.TestDataGenerator;
+import com.blu.imdg.example3.ValidateMessage;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+
+import java.io.IOException;
+
+/**
+ * Created by shamim on 25/12/16.
+ */
+public class DeployNodeSingleton {
+    private static final String VALIDATING_SERVICE = "validatingService";
+
+    public static void main(String[] args) throws IOException{
+        try (Ignite ignite = Ignition.start(CommonConstants.CLIENT_CONFIG)) {
+
+            System.out.println("Deploy Node-singleton service!!");
+            ignite.services().deployNodeSingleton(VALIDATING_SERVICE, new XsdValidatingServiceImpl());
+
+//            XsdValidatingService xsdValidatingService = ignite.services().serviceProxy(VALIDATING_SERVICE, XsdValidatingService.class, /*not-sticky*/false);
+//
+//            System.out.println("result=" + xsdValidatingService.isOk(new ValidateMessage("1", sample1, vaidateSchema, validateScript)));
+//            System.out.println("result2=" + xsdValidatingService.isOk(new ValidateMessage("2", sample2, vaidateSchema, validateScript)));
+//
+//            ignite.services().cancel(VALIDATING_SERVICE);
+        }
+
+    }
+}
--- chapters/chapter-dist-computing/src/main/java/com/blu/imdg/example7/RunXsdValidationService.java
+++ chapters/chapter-dist-computing/src/main/java/com/blu/imdg/example7/RunXsdValidationService.java
@@ -9,13 +9,12 @@ import org.apache.ignite.Ignition;
 import java.io.IOException;
 
 /**
- * Created by mikl on 07.08.16.
+ * Created by shamim on 25/12/16.
  */
-public class TestXsdValidatingService {
-
+public class RunXsdValidationService {
     private static final String VALIDATING_SERVICE = "validatingService";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         try (Ignite ignite = Ignition.start(CommonConstants.CLIENT_CONFIG)) {
 
             String sample1 = TestDataGenerator.getSample1();
@@ -23,15 +22,16 @@ public class TestXsdValidatingService {
             byte[] vaidateSchema = TestDataGenerator.getValidateSchema();
             String validateScript = TestDataGenerator.getValidateScript();
 
-            ignite.services().deployNodeSingleton(VALIDATING_SERVICE, new XsdValidatingServiceImpl());
+            System.out.println("Invoke remote service!!");
 
             XsdValidatingService xsdValidatingService = ignite.services().serviceProxy(VALIDATING_SERVICE, XsdValidatingService.class, /*not-sticky*/false);
 
             System.out.println("result=" + xsdValidatingService.isOk(new ValidateMessage("1", sample1, vaidateSchema, validateScript)));
             System.out.println("result2=" + xsdValidatingService.isOk(new ValidateMessage("2", sample2, vaidateSchema, validateScript)));
 
-            ignite.services().cancel(VALIDATING_SERVICE);
+            //ignite.services().cancel(VALIDATING_SERVICE);
         }
 
     }
+
 }
