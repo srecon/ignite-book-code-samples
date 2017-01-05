14af140e841970e7d65b7793d5933e07a5f7fe7c
shamim <shamimapache@gmail.com>
2017-01-05 22:01:51 +0300

    fix typos

--- chapters/chapter-bigdata-spark/ignite-spark-scala/src/main/scala/com/blu/imdg/SparkIgniteTest.scala
+++ chapters/chapter-bigdata-spark/ignite-spark-scala/src/main/scala/com/blu/imdg/SparkIgniteTest.scala
@@ -5,18 +5,18 @@ import org.apache.ignite.spark.{IgniteContext, IgniteRDD}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object RDDProducer extends App {
-  val conf = new SparkConf().setAppName("SparkIgnitePro")
+  val conf = new SparkConf().setAppName("SparkIgniteProducer")
   val sc = new SparkContext(conf)
   val ic = new IgniteContext[Int, Int](sc, () => new IgniteConfiguration())
-  val sharedRDD: IgniteRDD[Int, Int] = ic.fromCache("partitioned")
-  sharedRDD.savePairs(sc.parallelize(1 to 100000, 10).map(i => (i, i)))
+  val sharedRDD: IgniteRDD[Int, Int] = ic.fromCache("IgniteRDD")
+  sharedRDD.savePairs(sc.parallelize(1 to 1000, 10).map(i => (i, i)))
 }
 
 object RDDConsumer extends App {
-  val conf = new SparkConf().setAppName("SparkIgniteCon")
+  val conf = new SparkConf().setAppName("SparkIgniteConsume")
   val sc = new SparkContext(conf)
   val ic = new IgniteContext[Int, Int](sc, () => new IgniteConfiguration())
-  val sharedRDD = ic.fromCache("partitioned")
-  val lessThanTen = sharedRDD.filter(_._2 < 10)
-  println("The count is:::::::::::: "+lessThanTen.count())
+  val sharedRDD = ic.fromCache("IgniteRDD")
+  val lessThanTwenty = sharedRDD.filter(_._2 < 20)
+  println("The count is:::::::::::: "+lessThanTwenty.count())
 }
